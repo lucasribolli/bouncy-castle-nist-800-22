c4ef5a574524c0ce64d07d3a21b5cde77986250f
lucasribolli <lucasribolli@gmail.com>
2024-11-05 08:21:40 -0300

    implement 2.4.4, throwing exceptions when the sizes are wrong

--- src/test/java/br/unicamp/criptografia/hash_drbg/BouncyCastleHashDRBGTest.java
+++ src/test/java/br/unicamp/criptografia/hash_drbg/BouncyCastleHashDRBGTest.java
@@ -5,9 +5,8 @@ import org.apache.commons.math3.special.Gamma;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import java.security.InvalidParameterException;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Vector;
 
 import static br.unicamp.criptografia.hash_drbg.CryptoHelper.generatePersonalizationString;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -15,8 +14,10 @@ import static org.hamcrest.Matchers.greaterThanOrEqualTo;
 
 public class BouncyCastleHashDRBGTest {
     private static final Double BASE_P_VALUE = 0.01;
-    private static final String NIST_EXAMPLE_RANDOM_BITS = "11001001000011111101101010100010001000010110100" +
+    private static final String NIST_EXAMPLE_RANDOM_BITS_100_BITS = "11001001000011111101101010100010001000010110100" +
             "01100001000110100110001001100011001100010100010111000";
+    private static final String NIST_EXAMPLE_RANDOM_BITS_128_BITS = "11001100000101010110110001001100111000000000001" +
+            "001001101010100010001001111010110100000001101011111001100111001101101100010110010";
     private static BouncyCastleHashDRBG bouncyCastle;
     private static String bouncyCastleRandomBits;
 
@@ -31,7 +32,7 @@ public class BouncyCastleHashDRBGTest {
 
     @Test
     public void frequencyMonobitTest_NIST_Example() {
-        double pValue = getFrequencyMonobitPValue(NIST_EXAMPLE_RANDOM_BITS);
+        double pValue = getFrequencyMonobitPValue(NIST_EXAMPLE_RANDOM_BITS_100_BITS);
 
         assertThat(pValue, greaterThanOrEqualTo(BASE_P_VALUE));
     }
@@ -82,7 +83,7 @@ public class BouncyCastleHashDRBGTest {
 
     @Test
     public void frequencyTestWithinABlock_NIST_Example() {
-        double pValue = getBlockFrequencyPValue(NIST_EXAMPLE_RANDOM_BITS, 10);
+        double pValue = getBlockFrequencyPValue(NIST_EXAMPLE_RANDOM_BITS_100_BITS, 10);
         assertThat(pValue, greaterThanOrEqualTo(BASE_P_VALUE));
     }
 
@@ -144,7 +145,7 @@ public class BouncyCastleHashDRBGTest {
 
     @Test
     public void runsTest_NIST_Example() {
-        double pValue = getRunsTestPValue(NIST_EXAMPLE_RANDOM_BITS);
+        double pValue = getRunsTestPValue(NIST_EXAMPLE_RANDOM_BITS_100_BITS);
         assertThat(pValue, greaterThanOrEqualTo(BASE_P_VALUE));
     }
 
@@ -210,20 +211,39 @@ public class BouncyCastleHashDRBGTest {
     }
 
     @Test
-    public void testForTheLongestRunOfOnesInABlock_NIST_Example() {
+    public void testForTheLongestRunOfOnesInABlock_Bouncy_Castle() {
+        double pValue = getTestForTheLongestRunOfOnesInABlockPValue(bouncyCastleRandomBits, bouncyCastle.getBlockSize());
+        assertThat(pValue, greaterThanOrEqualTo(BASE_P_VALUE));
+    }
 
+    @Test
+    public void testForTheLongestRunOfOnesInABlock_NIST_Example() {
+        double pValue = getTestForTheLongestRunOfOnesInABlockPValue(NIST_EXAMPLE_RANDOM_BITS_128_BITS, 8);
+        assertThat(pValue, greaterThanOrEqualTo(BASE_P_VALUE));
     }
 
     private double getTestForTheLongestRunOfOnesInABlockPValue(String randomBits, int lengthOfEachBlock) {
+        int lengthOfBitString = randomBits.length();
+
         Integer[][] preSetMinimumLengthOfBitStringByLengthOfEachBlock = {
                 {128, 8},
                 {6272, 128},
                 {750000, (int) Math.pow(10, 4)}
         };
 
+        for(Integer[] preSet : preSetMinimumLengthOfBitStringByLengthOfEachBlock) {
+            int maxNumberOfBitsSegment = preSet[0];
+            if (lengthOfBitString < maxNumberOfBitsSegment) {
+                int maxBlockSizeSegment = preSet[1];
+                if (lengthOfEachBlock >= maxBlockSizeSegment) {
+                    throw new InvalidParameterException("Random bits has a wrong block size: (lengthOfBitString) "
+                            + lengthOfBitString + "; (lengthOfEachBlock) " + lengthOfEachBlock);
+                }
+            }
+        }
+
 
         // 2.4.4 (1)
-        int lengthOfBitString = randomBits.length();
         int nonOverlappingBlocks = getNonOverlappingBlocks(lengthOfBitString, lengthOfEachBlock);
         ArrayList<String> blocks = getBlocks(randomBits, nonOverlappingBlocks);
 
@@ -253,11 +273,58 @@ public class BouncyCastleHashDRBGTest {
         };
 
 
+        return testLongestRunOfOnes(randomBits, lengthOfEachBlock, nonOverlappingBlocks);
 
 
+//        return 0.0;
+    }
 
 
-        return 0.0;
+    public double testLongestRunOfOnes(String binarySequence, int blockSize, int numberOfBlocks) {
+        // Define expected probabilities based on NIST's test
+        int[] expectedRuns = {1, 2, 3, 4}; // Adjust as needed for larger blocks
+        double[] probabilities = {0.2148, 0.3672, 0.2305, 0.1875};
+
+        // Count occurrences of the longest runs
+        int[] observedRuns = new int[expectedRuns.length];
+        for (int i = 0; i < numberOfBlocks; i++) {
+            String block = binarySequence.substring(i * blockSize, (i + 1) * blockSize);
+            int longestRun = longestRunOfOnes(block);
+
+            // Map the longest run to the expected categories
+            if (longestRun >= 1 && longestRun <= 4) {
+                observedRuns[longestRun - 1]++;
+            } else if (longestRun > 4) {
+                observedRuns[3]++;
+            }
+        }
+
+        // Perform chi-squared test
+        double chiSquareStatisticObserved = 0.0;
+        for (int i = 0; i < expectedRuns.length; i++) {
+            double expectedCount = probabilities[i] * numberOfBlocks;
+            chiSquareStatisticObserved += Math.pow(observedRuns[i] - expectedCount, 2) / expectedCount;
+        }
+
+        return chiSquareStatisticObserved;
+    }
+
+    // Function to find the longest run of 1's in a block
+    public static int longestRunOfOnes(String block) {
+        int maxRun = 0;
+        int currentRun = 0;
+
+        for (char bit : block.toCharArray()) {
+            if (bit == '1') {
+                currentRun++;
+                if (currentRun > maxRun) {
+                    maxRun = currentRun;
+                }
+            } else {
+                currentRun = 0;
+            }
+        }
+        return maxRun;
     }
 
     private int getNonOverlappingBlocks(int lengthOfTheBitString, int lengthOfEachBlock) {
